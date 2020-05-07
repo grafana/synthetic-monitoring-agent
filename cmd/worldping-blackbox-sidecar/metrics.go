99215aec4822bfbd93138fe32405faee2318857a
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2020-05-07 15:16:28 -0600

    Rework code handling metrics
    Structure the http mux just like worldping-api, so that timing
    information is collected for all HTTP requests. Record these by method
    and resulting code.
    
    Use non-default prometheus registerer and gatherer so that metrics that
    are automatically registered by dependencies are not exposed (bbe, etcd,
    cortex, etc), as we don't actually use those dependencies.
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- cmd/worldping-blackbox-sidecar/metrics.go
+++ cmd/worldping-blackbox-sidecar/metrics.go
@@ -3,5 +3,17 @@ package main
 import "github.com/prometheus/client_golang/prometheus"
 
 func registerMetrics(r prometheus.Registerer) error {
-	return r.Register(prometheus.NewBuildInfoCollector())
+	if err := r.Register(prometheus.NewBuildInfoCollector()); err != nil {
+		return err
+	}
+
+	if err := r.Register(prometheus.NewGoCollector()); err != nil {
+		return err
+	}
+
+	if err := r.Register(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{})); err != nil {
+		return err
+	}
+
+	return nil
 }
