29442b0fa8fef40f8017968fdd568ff2cb18eabc
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2021-05-19 14:24:53 -0600

    Add feature package to implement feature flags
    You can create a feature.Collection, pass that to flagset.Var, and get a
    set of features from the command line. On the command line it
    understands a flag passed multiple times with different values or a flag
    passed one time with multiple values (separated by commas). E.g.:
    
    	my_progam --features foo --features bar
    	my_progam --features foo,bar
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

---
+++ internal/feature/feature.go
@@ -0,0 +1,72 @@
+// The feature package provides types and methods for working with
+// feature flags.
+package feature
+
+import (
+	"errors"
+	"sort"
+	"strings"
+)
+
+// ErrInvalidCollection is returned when you try to set a flag in an
+// invalid collection.
+var ErrInvalidCollection = errors.New("invalid feature collection")
+
+// Collection represents a set of feature flags.
+type Collection map[string]struct{}
+
+// NewCollection returns a correctly initialized Collection.
+func NewCollection() Collection {
+	return make(Collection)
+}
+
+// Set adds the value specified by s to the collection.
+//
+// The input s can be a single feature flag or multiple feature flags
+// separated by commas.
+func (c Collection) Set(s string) error {
+	if c == nil {
+		return ErrInvalidCollection
+	}
+
+	for _, elem := range strings.Split(s, ",") {
+		feature := strings.TrimSpace(elem)
+		if feature == "" {
+			continue
+		}
+		c[feature] = struct{}{}
+	}
+
+	return nil
+}
+
+// String returns an string representation of the collection.
+//
+// The returned value can be passed to Set to recreate an identical
+// collection.
+func (c Collection) String() string {
+	if c == nil {
+		return ""
+	}
+
+	values := make([]string, 0, len(c))
+	for elem := range c {
+		values = append(values, elem)
+	}
+
+	sort.Strings(values)
+
+	return strings.Join(values, ",")
+}
+
+// IsSet returns true if the provided name is part of the collection,
+// false otherwise.
+func (c Collection) IsSet(name string) bool {
+	if c == nil {
+		return false
+	}
+
+	_, found := c[name]
+
+	return found
+}
