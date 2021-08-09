c5d5eedf8cf8ac8552e5484a69cef7a363702787
Russ <8377044+rdubrock@users.noreply.github.com>
2021-08-09 13:12:43 -0800

    handle frequency/timeout validations in their own function

--- pkg/pb/synthetic_monitoring/checks_extra.go
+++ pkg/pb/synthetic_monitoring/checks_extra.go
@@ -155,26 +155,12 @@ func (c *Check) Validate() error {
 		return ErrInvalidCheckJob
 	}
 
-	// frequency must be in [1, 120] seconds
-	if c.Settings.Traceroute == nil {
-		if c.Frequency < 1*1000 || c.Frequency > 120*1000 {
-			return ErrInvalidCheckFrequency
-		}
-		// timeout must be in [1, 10] seconds, and it must be less than
-		// frequency (otherwise we can end up running overlapping
-		// checks)
-		if c.Timeout < 1*1000 || c.Timeout > 10*1000 || c.Timeout > c.Frequency {
-			return ErrInvalidCheckTimeout
-		}
+	if err := c.validateFrequency(); err != nil {
+		return err
 	}
-	if c.Settings.Traceroute != nil {
-		// We are hardcoding traceroute frequency and timeout until we can get data on what the boundaries should be
-		if c.Frequency != 120*1000 {
-			return ErrInvalidCheckFrequency
-		}
-		if c.Timeout != 30*1000 {
-			return ErrInvalidCheckTimeout
-		}
+
+	if err := c.validateTimeout(); err != nil {
+		return err
 	}
 
 	if err := validateLabels(c.Labels); err != nil {
@@ -245,6 +231,37 @@ func (c *Check) Validate() error {
 		}
 	}
 
+	return nil
+}
+
+func (c *Check) validateFrequency() error {
+	// frequency must be in [1, 120] seconds
+	if c.Settings.Traceroute != nil {
+		if c.Frequency != 120*1000 {
+			return ErrInvalidCheckFrequency
+		}
+	} else {
+		if c.Frequency < 1*1000 || c.Frequency > 120*1000 {
+			return ErrInvalidCheckFrequency
+		}
+	}
+	return nil
+}
+
+func (c *Check) validateTimeout() error {
+	if c.Settings.Traceroute != nil {
+		// We are hardcoding traceroute frequency and timeout until we can get data on what the boundaries should be
+		if c.Timeout != 30*1000 {
+			return ErrInvalidCheckTimeout
+		}
+	} else {
+		// timeout must be in [1, 10] seconds, and it must be less than
+		// frequency (otherwise we can end up running overlapping
+		// checks)
+		if c.Timeout < 1*1000 || c.Timeout > 10*1000 || c.Timeout > c.Frequency {
+			return ErrInvalidCheckTimeout
+		}
+	}
 	return nil
 }
 
