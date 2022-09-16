04eefadad1ddb336a08e13a1fb9a8f8631600c5d
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2022-09-16 17:07:45 -0600

    Add very simple code to publish k6 metrics and logs
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- internal/prober/k6/k6.go
+++ internal/prober/k6/k6.go
@@ -53,7 +53,7 @@ func (p Prober) Name() string {
 }
 
 func (p Prober) Probe(ctx context.Context, target string, registry *prometheus.Registry, logger logger.Logger) bool {
-	err := p.runner.Run(ctx)
+	err := p.runner.Run(ctx, registry, logger, p.logger)
 	if err != nil {
 		p.logger.Warn().Err(err).Msg("running probe")
 		return false
