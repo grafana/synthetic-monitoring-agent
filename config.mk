388322341babd024ed9d7ef55584d72d83dc5ccd
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2023-10-10 14:56:12 -0600

    Update Go version used to build agent
    We want 1.21 (and ideally >= 1.21.3)
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@
 
 DOCKER_TAG = grafana/synthetic-monitoring-agent
 
-GO_TOOLS_IMAGE := us.gcr.io/kubernetes-dev/go-tools:2023-10-04-v379665-2ea0c2a4f
+GO_TOOLS_IMAGE := us.gcr.io/kubernetes-dev/go-tools:2023-10-10-v384623-f09f9eb30
 
 PLATFORMS := $(sort $(HOST_OS)/$(HOST_ARCH) linux/amd64 linux/arm64 linux/arm)
