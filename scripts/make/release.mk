b11154f62ab210f6edbbe6fe38beef41bdd7dff9
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2023-10-31 18:31:07 -0600

    Fix: add k6 binary to release files
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- scripts/make/release.mk
+++ scripts/make/release.mk
@@ -3,14 +3,14 @@ GOPATH := $(shell go env GOPATH)
 GORELEASER := $(GOPATH)/bin/goreleaser
 # TODO: Upgrade goreleaser when Go version is upgraded past 1.17. Newer versions require Go 1.18 or 1.19
 $(GORELEASER):
-	go install github.com/goreleaser/goreleaser@v1.6.3 
+	go install github.com/goreleaser/goreleaser@v1.21.2 
 
 .PHONY: release
 release: $(GORELEASER) ## Build a release and publish it to Github.
 	$(S) echo "Building and publishing release files..."		
-	$(GORELEASER) release --rm-dist
+	$(GORELEASER) release --rm-dist $(GORELEASER_FLAGS)
 
 .PHONY: release-snapshot
 release-snapshot: $(GORELEASER) ## Build a snapshot release for testing (not published).
 	$(S) echo "Building release files..."		
-	$(GORELEASER) release --snapshot --rm-dist
+	$(GORELEASER) release --snapshot --rm-dist $(GORELEASER_FLAGS)
