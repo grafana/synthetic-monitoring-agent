2c194845356ebaa5db0c4c488029f72526ba4cf5
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2023-12-12 11:27:46 -0600

    Swtich to grafana-build-tools
    grafana-build-tools is a public container image. This makes it simpler
    for outside contributors to use the same build infrastructure as the CI
    system, which makes it simpler to reproduce and debug issues.
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@
 
 DOCKER_TAG = grafana/synthetic-monitoring-agent
 
-GO_TOOLS_IMAGE := us.gcr.io/kubernetes-dev/go-tools:2023-10-10-v384623-f09f9eb30
+GO_TOOLS_IMAGE := ghcr.io/grafana/grafana-build-tools:v0.4.0
 
-PLATFORMS := $(sort $(HOST_OS)/$(HOST_ARCH) linux/amd64 linux/arm64 linux/arm)
+PLATFORMS := $(sort $(HOST_OS)/$(HOST_ARCH) linux/amd64 linux/arm64)
