8de870cb0b8d06f132c1420efc5fa9be6115e1c4
ka3de <danijs12@hotmail.com>
2023-12-19 09:18:31 +0100

    Add TenantLimits
    Implements a new component that holds the logic to query or validate
    specific tenant limits, whether these are informed from the API
    communication or falling back to default limits.

---
+++ internal/limits/tenant.go
@@ -0,0 +1,98 @@
+package limits
+
+import (
+	"context"
+	"errors"
+
+	sm "github.com/grafana/synthetic-monitoring-agent/pkg/pb/synthetic_monitoring"
+)
+
+const (
+	// Tenant limits (defaults)
+
+	maxMetricLabels = sm.MaxMetricLabels
+	maxLogLabels    = sm.MaxLogLabels
+)
+
+var (
+	ErrTooManyMetricLabels = errors.New("too many metric labels")
+	ErrTooManyLogLabels    = errors.New("too many log labels")
+)
+
+type TenantProvider interface {
+	GetTenant(context.Context, *sm.TenantInfo) (*sm.Tenant, error)
+}
+
+// TenantLimits provides functionalities to query and validate metrics and log
+// labels limits for a particular tenant.
+type TenantLimits struct {
+	tp TenantProvider
+}
+
+func NewTenantLimits(tp TenantProvider) *TenantLimits {
+	return &TenantLimits{
+		tp,
+	}
+}
+
+// MetricLabels returns the metric labels limit for the specified tenant.
+func (tl *TenantLimits) MetricLabels(ctx context.Context, tenantID int64) (int, error) {
+	tenant, err := tl.tp.GetTenant(ctx, &sm.TenantInfo{
+		Id: tenantID,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	max := maxMetricLabels
+	if tenant.Limits != nil {
+		max = int(tenant.Limits.MaxMetricLabels)
+	}
+
+	return max, nil
+}
+
+// LogLabels returns the log labels limit for the specified tenant.
+func (tl *TenantLimits) LogLabels(ctx context.Context, tenantID int64) (int, error) {
+	tenant, err := tl.tp.GetTenant(ctx, &sm.TenantInfo{
+		Id: tenantID,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	max := maxLogLabels
+	if tenant.Limits != nil {
+		max = int(tenant.Limits.MaxLogLabels)
+	}
+
+	return max, nil
+}
+
+// ValidateMetricLabels validates the given number of metric labels against the specific tenant limits.
+func (tl *TenantLimits) ValidateMetricLabels(ctx context.Context, tenantID int64, n int) error {
+	max, err := tl.MetricLabels(ctx, tenantID)
+	if err != nil {
+		return err
+	}
+
+	if n > max {
+		return ErrTooManyMetricLabels
+	}
+
+	return nil
+}
+
+// ValidateLogLabels validates the given number of log labels against the specific tenant limits.
+func (tl *TenantLimits) ValidateLogLabels(ctx context.Context, tenantID int64, n int) error {
+	max, err := tl.LogLabels(ctx, tenantID)
+	if err != nil {
+		return err
+	}
+
+	if n > max {
+		return ErrTooManyLogLabels
+	}
+
+	return nil
+}
---
+++ internal/limits/tenant_test.go
@@ -0,0 +1,227 @@
+package limits
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	sm "github.com/grafana/synthetic-monitoring-agent/pkg/pb/synthetic_monitoring"
+	"github.com/stretchr/testify/require"
+)
+
+var errTestTenantNotFound = errors.New("tenant not found")
+
+type testTenantProvider struct {
+	tenants map[int64]*sm.Tenant
+}
+
+func (tp *testTenantProvider) GetTenant(ctx context.Context, ti *sm.TenantInfo) (*sm.Tenant, error) {
+	if t, ok := tp.tenants[ti.Id]; ok {
+		return t, nil
+	}
+	return nil, errTestTenantNotFound
+}
+
+var tp = &testTenantProvider{
+	tenants: map[int64]*sm.Tenant{
+		// Tenant with specific limits that override defaults
+		1: {
+			Id: 1,
+			Limits: &sm.TenantLimits{
+				MaxMetricLabels: 30,
+				MaxLogLabels:    25,
+			},
+		},
+		// Tenant without specific limits
+		2: {
+			Id:     2,
+			Limits: nil,
+		},
+	},
+}
+
+func TestMetricLabels(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name      string
+		tenantID  int
+		expLabels int
+		expErr    error
+	}{
+		{
+			name:      "specific tenant limits",
+			tenantID:  1,
+			expLabels: 30,
+		},
+		{
+			name:      "default tenant limits",
+			tenantID:  2,
+			expLabels: maxMetricLabels,
+		},
+		{
+			name:     "expect error",
+			tenantID: 3,
+			expErr:   errTestTenantNotFound,
+		},
+	}
+
+	var (
+		ctx = context.Background()
+		tl  = NewTenantLimits(tp)
+	)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ll, err := tl.MetricLabels(ctx, int64(tc.tenantID))
+			if err != nil {
+				require.Equal(t, tc.expErr, err)
+			}
+			require.Equal(t, tc.expLabels, ll)
+		})
+	}
+}
+
+func TestLogLabels(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name      string
+		tenantID  int
+		expLabels int
+		expErr    error
+	}{
+		{
+			name:      "specific tenant limits",
+			tenantID:  1,
+			expLabels: 25,
+		},
+		{
+			name:      "default tenant limits",
+			tenantID:  2,
+			expLabels: maxLogLabels,
+		},
+		{
+			name:     "expect error",
+			tenantID: 3,
+			expErr:   errTestTenantNotFound,
+		},
+	}
+
+	var (
+		ctx = context.Background()
+		tl  = NewTenantLimits(tp)
+	)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ll, err := tl.LogLabels(ctx, int64(tc.tenantID))
+			if err != nil {
+				require.Equal(t, tc.expErr, err)
+			}
+			require.Equal(t, tc.expLabels, ll)
+		})
+	}
+}
+
+func TestValidateMetricLabels(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		tenantID int
+		nLabels  int
+		expErr   error
+	}{
+		{
+			name:     "below tenant metric labels limit",
+			tenantID: 1,
+			nLabels:  25,
+		},
+		{
+			name:     "equal tenant metric labels limit",
+			tenantID: 1,
+			nLabels:  30,
+		},
+		{
+			name:     "below default metric labels limit",
+			tenantID: 2,
+			nLabels:  15,
+		},
+		{
+			name:     "over tenant metric labels limit",
+			tenantID: 1,
+			nLabels:  35,
+			expErr:   ErrTooManyMetricLabels,
+		},
+		{
+			name:     "over tenant metric labels limit",
+			tenantID: 2,
+			nLabels:  21,
+			expErr:   ErrTooManyMetricLabels,
+		},
+	}
+
+	var (
+		ctx = context.Background()
+		tl  = NewTenantLimits(tp)
+	)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tl.ValidateMetricLabels(ctx, int64(tc.tenantID), tc.nLabels)
+			require.Equal(t, tc.expErr, err)
+		})
+	}
+}
+
+func TestValidateLogLabels(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		tenantID int
+		nLabels  int
+		expErr   error
+	}{
+		{
+			name:     "below tenant log labels limit",
+			tenantID: 1,
+			nLabels:  21,
+		},
+		{
+			name:     "equal tenant log labels limit",
+			tenantID: 1,
+			nLabels:  25,
+		},
+		{
+			name:     "below default log labels limit",
+			tenantID: 2,
+			nLabels:  10,
+		},
+		{
+			name:     "over tenant log labels limit",
+			tenantID: 1,
+			nLabels:  29,
+			expErr:   ErrTooManyLogLabels,
+		},
+		{
+			name:     "over tenant log labels limit",
+			tenantID: 2,
+			nLabels:  18,
+			expErr:   ErrTooManyLogLabels,
+		},
+	}
+
+	var (
+		ctx = context.Background()
+		tl  = NewTenantLimits(tp)
+	)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tl.ValidateLogLabels(ctx, int64(tc.tenantID), tc.nLabels)
+			require.Equal(t, tc.expErr, err)
+		})
+	}
+}
