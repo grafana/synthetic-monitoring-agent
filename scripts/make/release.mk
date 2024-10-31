624bd985f8aa3357ae21fe5c4d0353a5328d9359
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2024-10-31 14:43:46 -0600

    Fix: rework package creation
    Instead of the weird workarounds with goreleaser, just use nfpm to
    create the packages.
    
    - This doesn't do signing.
    - This is not changing how the current release works, only adding
      targets and using them in the GitHub workflow.
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- scripts/make/release.mk
+++ scripts/make/release.mk
@@ -14,3 +14,54 @@ release: $(GORELEASER) ## Build a release and publish it to Github.
 release-snapshot: $(GORELEASER) ## Build a snapshot release for testing (not published).
 	$(S) echo "Building release files..."		
 	$(GORELEASER) release --snapshot --rm-dist $(GORELEASER_FLAGS)
+
+define package_template
+PACKAGE_TARGETS += package-$(1)-$(2)
+
+$(DISTDIR)/$(1)-$(2)/nfpm-input.yaml: $(DISTDIR)/version
+	'$(ROOTDIR)/scripts/package/create-parameters-file' '$$@' '$(1)' '$(2)' '$(BUILD_VERSION)' '$(ROOTDIR)' '$(DISTDIR)'
+
+$(DISTDIR)/$(1)-$(2)/nfpm.yaml : $(DISTDIR)/$(1)-$(2)/nfpm-input.yaml
+$(DISTDIR)/$(1)-$(2)/nfpm.yaml : $(ROOTDIR)/scripts/package/nfpm.yaml.template
+$(DISTDIR)/$(1)-$(2)/nfpm.yaml :
+	$(S) mkdir -p $(DISTDIR)/$(1)-$(2)
+	$(S) gomplate \
+		--context '.=file://$(DISTDIR)/$(1)-$(2)/nfpm-input.yaml' \
+		--file '$(ROOTDIR)/scripts/package/nfpm.yaml.template' \
+		> '$$@'
+
+package-deb-$(1)-$(2) : $(DISTDIR)/$(1)-$(2)/nfpm.yaml $(DISTDIR)/changelog.yaml
+	$(S) nfpm package \
+		--config $(DISTDIR)/$(1)-$(2)/nfpm.yaml \
+		--packager deb \
+		--target $(DISTDIR)/$(1)-$(2)/
+
+package-rpm-$(1)-$(2) : $(DISTDIR)/$(1)-$(2)/nfpm.yaml $(DISTDIR)/changelog.yaml
+	$(S) nfpm package \
+		--config $(DISTDIR)/$(1)-$(2)/nfpm.yaml \
+		--packager rpm \
+		--target $(DISTDIR)/$(1)-$(2)/
+
+package-$(1)-$(2) : package-deb-$(1)-$(2) package-rpm-$(1)-$(2)
+	@true
+
+package : package-$(1)-$(2)
+endef
+
+$(foreach BUILD_PLATFORM,$(PLATFORMS), \
+	$(eval $(call package_template,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))))))
+
+$(DISTDIR)/changelog.yaml: $(DISTDIR)/version
+	$(S) chglog init \
+		--conventional-commits \
+		--exclude-merge-commits \
+		--owner 'Grafana Labs <support@grafana.com>' \
+		--config-file '$(ROOTDIR)/scripts/package/chglog.yaml' \
+		--output '$@'
+
+.PHONY: package
+package:
+	@true
+
+package-native : package-$(HOST_OS)-$(HOST_ARCH)
+	@true
