bc5e6d6d2187a661c1812ea71b3c5d389024cd42
Dougal Matthews <dougal@grafana.com>
2025-01-29 14:26:53 +0000

    fix: increase Scripted and Browser max timeout to 180s

--- pkg/pb/synthetic_monitoring/checks_extra.go
+++ pkg/pb/synthetic_monitoring/checks_extra.go
@@ -148,7 +148,7 @@ const (
 	minCheckTimeout      = minCheckFrequency
 	MaxCheckTimeout      = 1 * 60 * 1000   // Maximum value for the check's timeout (1 minute).
 	minScriptedTimeout   = minCheckTimeout // Minimum timeout for scripted checks (1 second).
-	maxScriptedTimeout   = 120 * 1000      // Maximum timeout for scripted checks (120 second).
+	maxScriptedTimeout   = 180 * 1000      // Maximum timeout for scripted checks (180 second).
 	minTracerouteTimeout = 30 * 1000       // Minimum timeout for traceroute checks (30 second).
 	maxTracerouteTimeout = 30 * 1000       // Minimum timeout for traceroute checks (30 second).
 )
