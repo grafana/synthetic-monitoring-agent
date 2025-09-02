c237b9969fdcde759be7a8e9c67364b6842377c5
Nadia Santalla <nadia@santalla.io>
2025-09-02 16:58:36 +0200

    chore: k6runner/env: set `K6_AUTO_EXTENSION_RESOLUTION=false` by default

--- internal/k6runner/env_test.go
+++ internal/k6runner/env_test.go
@@ -8,7 +8,7 @@ import (
 func TestEnv(t *testing.T) {
 	t.Parallel()
 
-	t.Run("adds k6 browser log default", func(t *testing.T) {
+	t.Run("adds default variables", func(t *testing.T) {
 		t.Parallel()
 
 		osenv := []string{"SOMETHING=else", "FOO=bar"}
@@ -17,15 +17,22 @@ func TestEnv(t *testing.T) {
 		if !slices.Contains(modified, "K6_BROWSER_LOG=info") {
 			t.Fatalf("Expected env to contain browser log info")
 		}
+		if !slices.Contains(modified, "K6_AUTO_EXTENSION_RESOLUTION=false") {
+			t.Fatalf("Expected env to contain K6_AUTO_EXTENSION_RESOLUTION")
+		}
 	})
 
 	t.Run("does not modify existing value", func(t *testing.T) {
 		t.Parallel()
 
-		osenv := []string{"SOMETHING=else", "FOO=bar", "K6_BROWSER_LOG=debug"}
+		osenv := []string{"SOMETHING=else", "FOO=bar", "K6_BROWSER_LOG=debug", "K6_AUTO_EXTENSION_RESOLUTION=true"}
 		modified := k6Env(osenv)
 
 		if !slices.Contains(modified, "K6_BROWSER_LOG=debug") {
+			t.Fatalf("Expected env to contain original variable")
+		}
+
+		if !slices.Contains(modified, "K6_AUTO_EXTENSION_RESOLUTION=true") {
 			t.Fatalf("Expected env to contain original variable")
 		}
 
