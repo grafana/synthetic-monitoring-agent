d5d37dbfdf956fcbf434bbacda0d8a44cc169f45
Nadia Santalla <nadia@santalla.io>
2025-12-12 12:51:13 +0100

    feat: k6runner: allow k6 version repository to match constraints

--- internal/k6runner/version/repository_test.go
+++ internal/k6runner/version/repository_test.go
@@ -1,6 +1,7 @@
 package version_test
 
 import (
+	"errors"
 	"slices"
 	"testing"
 
@@ -54,5 +55,71 @@ func TestVersions(t *testing.T) {
 		if !slices.ContainsFunc(versions, func(v version.Entry) bool { return ev == v.Version.String() }) {
 			t.Fatalf("Expected version %q not found in %v", ev, versions)
 		}
+	}
+}
+
+func TestBinaryFor(t *testing.T) {
+	t.Parallel()
+
+	repo := version.Repository{
+		Root: "./testdata/",
+	}
+
+	// Testdata folder contains k6 mocks matching v1.2.3 and v2.0.0
+
+	for _, tc := range []struct {
+		name        string
+		constraint  string
+		expected    string
+		expectError error
+	}{
+		{
+			name:       "Matches anything to most recent",
+			constraint: "*",
+			expected:   "testdata/sm-k6-v2",
+		},
+		{
+			name:       "Matches caret v1 to v1",
+			constraint: "^v1.0.0",
+			expected:   "testdata/sm-k6-v1",
+		},
+		{
+			name:       "Matches caret 1 without v to v1",
+			constraint: "^1.0.0",
+			expected:   "testdata/sm-k6-v1",
+		},
+		{
+			name:       "Matches caret v2 to v2",
+			constraint: "^v2.0.0",
+			expected:   "testdata/sm-k6-v2",
+		},
+		{
+			name:       "Matches tilde v1 to minor",
+			constraint: "~v1.2.0",
+			expected:   "testdata/sm-k6-v1",
+		},
+		{
+			name:       "Matches exact version",
+			constraint: "v1.2.3",
+			expected:   "testdata/sm-k6-v1",
+		},
+		{
+			name:        "Errors for unsatisfiable version",
+			constraint:  "^v9.0.0",
+			expectError: version.ErrUnsatisfiable,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := repo.BinaryFor(tc.constraint)
+			if !errors.Is(err, tc.expectError) {
+				t.Fatalf("expected error to be %q, got: %q", tc.expectError, err)
+			}
+
+			if actual != tc.expected {
+				t.Fatalf("expected binary to be %q, got %q", tc.expected, actual)
+			}
+		})
 	}
 }
