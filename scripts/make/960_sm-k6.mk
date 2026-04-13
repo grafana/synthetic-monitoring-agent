f7d4561e04282d976deb59fab36261caf5d5dc13
ka3de <danijs12@hotmail.com>
2026-04-13 09:21:03 +0200

    feat: Add support for xk6-sm v2
    Updates build and release files in order to include xk6-sm v2 binary
    bundled within the same directory as v1.
    Also updates renovate configuration in order to keep both dependencies
    up to date.

--- scripts/make/960_sm-k6.mk
+++ scripts/make/960_sm-k6.mk
@@ -4,18 +4,31 @@ XK6_PLATFORMS := $(filter-out linux/arm,$(PLATFORMS)) darwin/arm64 darwin/amd64
 sm-k6:
 	@true
 
-define sm-k6-binary
+define sm-k6-v1-binary
 $(DISTDIR)/$(1)-$(2)/k6-v1:
 	mkdir -p "$(DISTDIR)/$(1)-$(2)"
 	# Renovate updates the following line. Keep its syntax as it is.
-	curl -sSL https://github.com/grafana/xk6-sm/releases/download/v0.6.20/sm-k6-$(1)-$(2) -o "$$@"
+	curl -sSL https://github.com/grafana/xk6-sm/releases/download/v0.6.20/sm-k6-$(1)-$(2) -o "$$@" # k6-v1
 	chmod +x "$$@"
 
 sm-k6: $(DISTDIR)/$(1)-$(2)/k6-v1
 endef
 
+define sm-k6-v2-binary
+$(DISTDIR)/$(1)-$(2)/k6-v2:
+	mkdir -p "$(DISTDIR)/$(1)-$(2)"
+	# Renovate updates the following line. Keep its syntax as it is.
+	curl -sSL https://github.com/grafana/xk6-sm/releases/download/v2.0.0/sm-k6-$(1)-$(2) -o "$$@" # k6-v2
+	chmod +x "$$@"
+
+sm-k6: $(DISTDIR)/$(1)-$(2)/k6-v2
+endef
+
 $(foreach BUILD_PLATFORM,$(XK6_PLATFORMS), \
-	$(eval $(call sm-k6-binary,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))))))
+	$(eval $(call sm-k6-v1-binary,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))))))
+
+$(foreach BUILD_PLATFORM,$(XK6_PLATFORMS), \
+	$(eval $(call sm-k6-v2-binary,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))))))
 
 .PHONY: sm-k6-native
-sm-k6-native: $(DISTDIR)/$(HOST_OS)-$(HOST_ARCH)/k6-v1
+sm-k6-native: $(DISTDIR)/$(HOST_OS)-$(HOST_ARCH)/k6-v1 $(DISTDIR)/$(HOST_OS)-$(HOST_ARCH)/k6-v2
