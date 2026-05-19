5c9e31a781d350c3b80f910243bdc9ebb46f9c24
Daniel Jiménez <danijs12@hotmail.com>
2026-05-19 14:18:11 +0200

    feat: Add support for xk6-sm v2 (#1771)
    * feat: Add support for xk6-sm v2
    
    Updates build and release files in order to include xk6-sm v2 binary
    bundled within the same directory as v1.
    Also updates renovate configuration in order to keep both dependencies
    up to date.
    
    * chore: Skip multihttp TestSettingsToScript for xk6-sm v2
    
    multihttp checks are currently forced to run with xk6-sm v1 only.
    
    * fix: Simplify renovate setup for updating k6 versions
    
    Inject variables into the docker files and makefiles so that files are
    easier to read. Strictly speaking it's not necessary to use the exact
    same pattern in both places, as you can have multiple managers for the
    same dependency, but since it's not that terrible to deferred variables
    in the makefile, which happen to use a syntax that is close enough to
    Docker's, use that.
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>
    
    ---------
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>
    Co-authored-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- scripts/make/960_sm-k6.mk
+++ scripts/make/960_sm-k6.mk
@@ -1,21 +1,36 @@
 XK6_PLATFORMS := $(filter-out linux/arm,$(PLATFORMS)) darwin/arm64 darwin/amd64
 
+K6_V1_VERSION=v1.1.0
+K6_V2_VERSION=v2.0.0
+
 .PHONY: sm-k6
 sm-k6:
 	@true
 
+.PHONY: sm-k6-native
+sm-k6-native:
+	@true
+
+# Args:
+# 1: OS
+# 2: Arch
+# 3: Tag
+# 4: Version
 define sm-k6-binary
-$(DISTDIR)/$(1)-$(2)/k6-v1:
+$(DISTDIR)/$(1)-$(2)/k6-$(3):
 	mkdir -p "$(DISTDIR)/$(1)-$(2)"
-	# Renovate updates the following line. Keep its syntax as it is.
-	curl -sSL https://github.com/grafana/xk6-sm/releases/download/v1.1.0/sm-k6-$(1)-$(2) -o "$$@"
+	curl -sSL https://github.com/grafana/xk6-sm/releases/download/$(4)/sm-k6-$(1)-$(2) -o "$$@"
 	chmod +x "$$@"
 
-sm-k6: $(DISTDIR)/$(1)-$(2)/k6-v1
+sm-k6: $(DISTDIR)/$(1)-$(2)/k6-$(3)
+
+ifeq ($(HOST_OS)-$(HOST_ARCH),$(1)-$(2))
+sm-k6-native: $(DISTDIR)/$(1)-$(2)/k6-$(3)
+endif
 endef
 
 $(foreach BUILD_PLATFORM,$(XK6_PLATFORMS), \
-	$(eval $(call sm-k6-binary,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))))))
+	$(eval $(call sm-k6-binary,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))),v1,$(K6_V1_VERSION))))
 
-.PHONY: sm-k6-native
-sm-k6-native: $(DISTDIR)/$(HOST_OS)-$(HOST_ARCH)/k6-v1
+$(foreach BUILD_PLATFORM,$(XK6_PLATFORMS), \
+	$(eval $(call sm-k6-binary,$(word 1,$(subst /, ,$(BUILD_PLATFORM))),$(word 2,$(subst /, ,$(BUILD_PLATFORM))),v2,$(K6_V2_VERSION))))
